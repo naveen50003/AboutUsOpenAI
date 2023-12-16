c15861408592b5dcd383541ab4ef63cf656e3d10
Karumuru Navaneeth <naveen50003@gmail.com>
2023-12-16 22:41:41 +0530

    first commit

---
+++ ice_breaker.py
@@ -0,0 +1,65 @@
+from typing import Tuple
+
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+from third_parties.linkedin import scrape_linedin_profile
+from third_parties.twitter import scrape_user_tweets
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
+
+def ice_break(name: str) -> Tuple[PersonIntel, str, str]:
+
+  print("Entered ice breaker method")
+  linkedin_profile_url = linkedin_lookup_agent(name=name)
+
+  summary_template ="""
+       given the Linkedin information {information} about a person from I want you to create:
+         1. a short summary
+         2. two interesting facts about them
+         3. A topic that may interest them
+         4. 2 creative Ice breakers to open a conversation with them
+         \n{format_instructions}
+  """
+  # reservation_template = '''
+  #   Book us a nice table for two this Friday at 6:00 PM. 
+  #   Choose any cuisine, it doesn't matter. Send the confirmation by email.
+
+  #   Our location is: {query}
+
+  #   Format instructions:
+  #   {format_instructions}
+  # '''
+
+  print(person_intel_parser.get_format_instructions())
+  summary_prompt_template = PromptTemplate(
+                              input_variables=["information"], 
+                              template=summary_template,
+                              partial_variables={
+                                "format_instructions": person_intel_parser.get_format_instructions()
+                              }
+                            )
+
+  print(summary_prompt_template)
+  llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
+
+  chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+  linkedin_data = scrape_linedin_profile(linkedin_profile_url=linkedin_profile_url)
+
+  result = chain.run(information=linkedin_data)
+
+  print("+++++++++")
+  print(result)
+  print(person_intel_parser.parse(result))
+  # return result
+  return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url"), linkedin_profile_url
+
+# if __name__ == '__main__':
+#   print("Hello LangChain")
+#   result = ice_break("Kalvakuntla Taraka Rama Rao")
+#   #print(result)
+#   #print(scrape_user_tweets(username="naveenkarumuru"))
+
+
---
+++ third_parties/linkedin.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+def scrape_linedin_profile(linkedin_profile_url: str):
+  """scrape information from LinkedIn profiles,
+  Manually scrape the information from the LinkedIn profile"""
+  api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+  headers = {'Authorization': 'Bearer ' + os.environ["LINKEDIN_API_KEY"]}
+
+  # response = requests.get(api_endpoint,
+  #                       params={"url": linkedin_profile_url},
+  #                       headers=headers)
+  
+  # KTR
+  # response = requests.get('https://gist.githubusercontent.com/naveen50003/ae987da32ccbf4597bf7b539ece9fc3e/raw/8fe56916bfba1c676a6941b27695d3a7e6894ef5/KTR.json') 
+
+  # Navaneeth
+  # response = requests.get('https://gist.githubusercontent.com/naveen50003/486402b3bc3c865cefd34b21deed3b2a/raw/929a920f66b89f6dbf6cd2a78a1e73abad1c77d8/navaneeth-karu.json')
+
+  # Harrison Chase
+  # response = requests.get('https://gist.githubusercontent.com/naveen50003/e3a79c81905540045996e2d0bea8488b/raw/7abbc46a689351858b360069d6475f569fe807d4/harrison-chase.json')
+
+  # Eden Marco
+  response = requests.get('https://gist.githubusercontent.com/naveen50003/c8fed8a7e1ffcfa7ba0b5f69d06cf72f/raw/dcbcc43f444a13d72e03f3babd3159157e156e9c/eden-marco.json')
+  
+  """ Clean the empty fields to avoid succeeding Lang chain model(chat GPT) tokes limits=4096 tokes"""
+
+  data = response.json()
+  data = {
+    k: v
+    for k,v in data.items()
+    if v not in ([], "", "", None)
+      and k not in ["people_also_viewed","certifications"]
+  }
+  if data.get("groups"):
+    for group_dic in data.get("groups"):
+      group_dic.pop("profile_pic_url")
+
+  return data
+
