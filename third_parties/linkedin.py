585aa7255480fd5e5b247e7277f124d9ebe4fede
Karumuru Navaneeth <naveen50003@gmail.com>
2023-12-17 23:16:16 +0530

    made changes in UI and added KTR response

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -15,7 +15,7 @@ def scrape_linedin_profile(linkedin_profile_url: str):
   #                       headers=headers)
   
   # KTR
-  # response = requests.get('https://gist.githubusercontent.com/naveen50003/ae987da32ccbf4597bf7b539ece9fc3e/raw/8fe56916bfba1c676a6941b27695d3a7e6894ef5/KTR.json') 
+  response = requests.get('https://gist.githubusercontent.com/naveen50003/ae987da32ccbf4597bf7b539ece9fc3e/raw/3cdd62261436b10980ba3ee16dcc643f2af54067/KTR.json') 
 
   # Navaneeth
   # response = requests.get('https://gist.githubusercontent.com/naveen50003/486402b3bc3c865cefd34b21deed3b2a/raw/929a920f66b89f6dbf6cd2a78a1e73abad1c77d8/navaneeth-karu.json')
@@ -24,7 +24,7 @@ def scrape_linedin_profile(linkedin_profile_url: str):
   # response = requests.get('https://gist.githubusercontent.com/naveen50003/e3a79c81905540045996e2d0bea8488b/raw/7abbc46a689351858b360069d6475f569fe807d4/harrison-chase.json')
 
   # Eden Marco
-  response = requests.get('https://gist.githubusercontent.com/naveen50003/c8fed8a7e1ffcfa7ba0b5f69d06cf72f/raw/dcbcc43f444a13d72e03f3babd3159157e156e9c/eden-marco.json')
+  # response = requests.get('https://gist.githubusercontent.com/naveen50003/c8fed8a7e1ffcfa7ba0b5f69d06cf72f/raw/dcbcc43f444a13d72e03f3babd3159157e156e9c/eden-marco.json')
   
   """ Clean the empty fields to avoid succeeding Lang chain model(chat GPT) tokes limits=4096 tokes"""
 
